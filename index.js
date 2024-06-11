83433bd59b10e9ce324a0fdb4ff1ccb82bee0600
Piyush Dubey <piyush115dubey@gmail.com>
2024-06-11 22:12:12 +0530

    API for generate

---
+++ index.js
@@ -0,0 +1,30 @@
+// app.js
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/generate', async (req, res) => {
+    const { diagram } = req.body;
+    try {
+        const response = await axios.post('https://kroki.io/plantuml/svg', diagram, {
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+          responseType: 'arraybuffer',
+        });
+        res.set('Content-Type', 'image/svg+xml');
+        res.send(response.data);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Error generating diagram');
+      }
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
