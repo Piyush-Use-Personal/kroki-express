026c23007f7b04984e45660db7592b9fa15ebe1c
Piyush Dubey <piyush115dubey@gmail.com>
2024-06-25 19:37:45 +0530

    Added dotenv port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // app.js
 const express = require('express');
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
