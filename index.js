8f30b10063efffb6ae461abef284b9c9573bd31f
Piyush Dubey <piyush115dubey@gmail.com>
2024-06-26 22:58:07 +0530

    Fixed mermaid code to xml generation

--- index.js
+++ index.js
@@ -11,22 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/generate', async (req, res) => {
-    const { diagram } = req.body;
-    try {
-        const response = await axios.post('https://kroki.io/plantuml/svg', diagram, {
-          headers: {
-            'Content-Type': 'text/plain',
-          },
-          responseType: 'arraybuffer',
-        });
-        res.set('Content-Type', 'image/svg+xml');
-        res.send(response.data);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Error generating diagram');
-      }
+  const { diagram } = req.body;
+  try {
+    const payload = {
+      diagram_source: diagram
+    };
+
+    const response = await axios.post(`https://kroki.io/mermaid/svg`, payload);
+    res.set('Content-Type', 'image/svg+xml');
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error generating diagram');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
